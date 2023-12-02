7303ea164a3bccf99653567159f67b35def107af
Noel <noel@seocontent.ai>
2023-12-02 21:20:30 +0100

    Move json-data to public folder

--- src/lib/api/preferenceAPI.ts
+++ src/lib/api/preferenceAPI.ts
@@ -1,10 +1,12 @@
 import axios from "axios";
 
 const preferenceAPI = axios.create({
-  baseURL: "http://localhost:3500",
+  baseURL: import.meta.env.PROD
+    ? "https://dev.d26ohjimvrz87s.amplifyapp.com/"
+    : "http://localhost:5173",
 });
 
-export const preferenceUrlEndpoint = "/data";
+export const preferenceUrlEndpoint = "/json-data.json";
 
 export const getPreferencesData = async () => {
   const response = await preferenceAPI.get(preferenceUrlEndpoint);
