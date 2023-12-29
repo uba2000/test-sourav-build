0e2cdf31aedd73bf53a22eb769fa6ae88601d3d5
Noel <noel@seocontent.ai>
2023-12-29 07:16:49 +0100

    Update rating logic

--- src/components/StarRatingComponent/StarRatingComponent.tsx
+++ src/components/StarRatingComponent/StarRatingComponent.tsx
@@ -4,12 +4,12 @@ import RatingStarIcon from '../../assets/product-rating-start.svg'
 import RatingStarDisabledIcon from '../../assets/product-rating-start-disabled.svg'
 import { Fragment } from 'react';
 
-function StarRatingComponent({ size = 12, star_rating = 0 }: { size?: number; star_rating?: number}) {
+function StarRatingComponent({ size = 12, star_rating = 0 }: { size?: number; star_rating?: number }) {
   return (
     <div className="flex gap-[2px]">
       {[1, 2, 3, 4, 5].map((d) => (
         <Fragment key={d}>
-          <ImageFigure icon={d >= star_rating ? RatingStarDisabledIcon : RatingStarIcon} width={size} />
+          <ImageFigure icon={d > star_rating ? RatingStarDisabledIcon : RatingStarIcon} width={size} />
         </Fragment>
       ))}
     </div>
