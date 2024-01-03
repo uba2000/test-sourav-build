d03756b8ae961b36f61195b3231a85af02f245e3
Noel <noel@seocontent.ai>
2024-01-03 14:43:34 +0100

    Remove test stream route

--- src/lib/hooks/useRouterRoutes.tsx
+++ src/lib/hooks/useRouterRoutes.tsx
@@ -11,8 +11,6 @@ import CompareComponents from '../../pages/build-pc/build-scratch/CompareCompone
 import MyBuild from '../../pages/build-pc/build-scratch/MyBuild/MyBuild';
 import PreconfigedSystems from '../../pages/build-pc/preconfiged-systems/PreconfigedSystems';
 import ResetSession from '../../pages/reset-session/ResetSession';
-import Page3D from '../../pages/page-3d-build';
-// import Page3D from '../../pages/page-3d-build';
 
 const BuildPreference = React.lazy(() => import('../../pages/build-pc/preference/BuildPreference'));
 
@@ -39,10 +37,10 @@ function useRouterRoutes() {
           path: RouteNames.buildPCMyBuild,
           element: <MyBuild />
         },
-        {
-          path: RouteNames.testStream,
-          element: <Page3D />
-        },
+        // {
+        //   path: RouteNames.testStream,
+        //   element: <Page3D />
+        // },
         {
           path: `${RouteNames.buildChooseComponent}/:category_slug`,
           element: <ChooseComponents />
