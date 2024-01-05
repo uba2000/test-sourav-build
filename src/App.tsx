8f6e9ff68193b757b297b17a6dc9ca7048047793
Noel <noel@seocontent.ai>
2024-01-05 15:18:24 +0100

    Retailer logo goes to home page

--- src/App.tsx
+++ src/App.tsx
@@ -2,34 +2,15 @@ import { RouterProvider } from 'react-router-dom';
 import useRouterRoutes from './lib/hooks/useRouterRoutes';
 import React from 'react';
 import SuspenseScreen from './components/SuspenseScreen/SuspenseScreen';
-import { use100vh } from './components/Widgets/Div100vh';
-import RetailerLogo from './assets/dummy-retailer-logo.svg?react'
 
 // App.tsx
 function App() {
   const router = useRouterRoutes()
-  const height = use100vh();
   console.log('v1.0.0');
 
   return (
     <React.Suspense fallback={<SuspenseScreen />}>
-      <div className="App">
-        <div className='pb-[2px]'>
-          <div className='flex mb-[2px]'>
-            <div className="py-3 px-5 min-w-[212px] flex justify-center bg-intel-e-gray-s1">
-              <RetailerLogo />
-            </div>
-            <div className="flex-1 bg-[#808080]" />
-          </div>
-          {/*  */}
-          <div className="bg-[#AEAEAE] h-[30px]" />
-        </div>
-        <div style={{ height: height ? height - 94 as string | number : '100vh' }} className='flex flex-col'>
-          <div className='overflow-y-auto scroll-smooth flex flex-col max-h-full flex-1' id="app_container">
-            <RouterProvider router={router} />
-          </div>
-        </div>
-      </div>
+      <RouterProvider router={router} />
     </React.Suspense>
   );
 }
--- src/components/Wrappers/MainWrapper.tsx
+++ src/components/Wrappers/MainWrapper.tsx
@@ -4,6 +4,8 @@ import BuildPCContextWrapper from './BuildPCContextWrapper';
 import RouteNames from '../../lib/utils/routenames';
 import useBuildPCContext from '../../lib/hooks/contextHooks/useBuildPCContext';
 import { useEffect } from 'react';
+import { use100vh } from '../Widgets/Div100vh';
+import RetailerLogo from '../../assets/dummy-retailer-logo.svg?react'
 
 const normalFlow = [{ path: "/" }]
 const preferenceFlow = [{ path: RouteNames.buildPreferenceIndex }]
@@ -14,21 +16,36 @@ export default function MainWrapper() {
   const isOnNormalFlow = matchRoutes(normalFlow, location)
   const isOnPreferenceFlow = matchRoutes(preferenceFlow, location)
   const isOnBuildFlow = matchRoutes(buildFlow, location)
+  const height = use100vh();
 
   return (
     <BuildPCContextWrapper>
-      <ValidateBuildHasData>
-        <div
-          className={clsx(
-            "min-h-full flex flex-col flex-grow overflow-hidden flex-1",
-            { "bg-gaming-navy": isOnNormalFlow || !isOnPreferenceFlow || !isOnBuildFlow },
-            { "bg-[radial-gradient(73.61%_80.84%_at_23.17%_18.42%,rgba(113,180,230,0.40)_0%,rgba(76,72,143,0.22)_27.09%,rgba(37,35,99,0.40)_45.19%,rgba(37,35,99,0.40)_65.68%,rgba(0,0,0,0.40)_89.96%),linear-gradient(180deg,#030009_0%,#0C0021_30.73%,#0C0021_69.27%,#030008_98.96%)]": isOnPreferenceFlow },
-            { "bg-gaming-navy": isOnBuildFlow },
-          )}
-        >
-          <Outlet />
+      <div className="App">
+        <div className='pb-[2px]'>
+          <div className='flex mb-[2px]'>
+            <RetailerIcon />
+            <div className="flex-1 bg-[#808080]" />
+          </div>
+          {/*  */}
+          <div className="bg-[#AEAEAE] h-[30px]" />
         </div>
-      </ValidateBuildHasData>
+        <div style={{ height: height ? height - 94 as string | number : '100vh' }} className='flex flex-col'>
+          <div className='overflow-y-auto scroll-smooth flex flex-col max-h-full flex-1' id="app_container">
+            <ValidateBuildHasData>
+              <div
+                className={clsx(
+                  "min-h-full flex flex-col flex-grow overflow-hidden flex-1",
+                  { "bg-gaming-navy": isOnNormalFlow || !isOnPreferenceFlow || !isOnBuildFlow },
+                  { "bg-[radial-gradient(73.61%_80.84%_at_23.17%_18.42%,rgba(113,180,230,0.40)_0%,rgba(76,72,143,0.22)_27.09%,rgba(37,35,99,0.40)_45.19%,rgba(37,35,99,0.40)_65.68%,rgba(0,0,0,0.40)_89.96%),linear-gradient(180deg,#030009_0%,#0C0021_30.73%,#0C0021_69.27%,#030008_98.96%)]": isOnPreferenceFlow },
+                  { "bg-gaming-navy": isOnBuildFlow },
+                )}
+              >
+                <Outlet />
+              </div>
+            </ValidateBuildHasData>
+          </div>
+        </div>
+      </div>
     </BuildPCContextWrapper>
   )
 }
@@ -55,4 +72,13 @@ function ValidateBuildHasData({ children }: IValidateBuildHasData) {
   }, [isOnBuildRoutes])
 
   return children
+}
+
+function RetailerIcon() {
+  const { resetApp } = useBuildPCContext();
+  return (
+    <div onClick={() => resetApp()} className="py-3 px-5 min-w-[212px] flex justify-center bg-intel-e-gray-s1">
+      <RetailerLogo />
+    </div>
+  )
 }
